6579d6f65f9f63f03a3b4762750c2068d5732188
Guilherme Braga <bbraga.guilherme@gmail.com>
2025-07-21 12:03:40 -0300

    Changed app.js to inside src folder

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 // Routes Imports
-import mainRoutes from "./src/routes/main.route.js"
+import mainRoutes from "./routes/main.route.js"
 
 // App configs
-import app from "./src/config/express.config.js";
-import setupHandlebars from "./src/config/handlebars.config.js";
+import app from "./config/express.config.js";
+import setupHandlebars from "./config/handlebars.config.js";
 import dotenv from "dotenv"
-import setupBodyParser from "./src/config/bodyparser.config.js";
-import setupGeneralSettings from "./src/config/generalSettings.config.js";
-import setupMongoose from "./src/config/mongoose.config.js";
+import setupBodyParser from "./config/bodyparser.config.js";
+import setupGeneralSettings from "./config/generalSettings.config.js";
+import setupMongoose from "./config/mongoose.config.js";
 
-import { checkExpiresDocs } from "./src/jobs/checkExpiresDocs.job.js";
+import { checkExpiresDocs } from "./jobs/checkExpiresDocs.job.js";
 
 dotenv.config()
 
